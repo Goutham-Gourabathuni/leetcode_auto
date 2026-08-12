ce3838f7d02adb37a71bf9fd299b7f58d13b9d27
Goutham G <156352020+Goutham-Gourabathuni@users.noreply.github.com>
2026-08-12 12:50:52 +0530

    Time: 5 ms (17.46%) | Memory: 19.6 MB (13.73%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.py
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.py
@@ -6,4 +6,9 @@ class Solution:
         for char in sentence:
             char_list.append(char)
         new_list = [int(x) for x in char_list]
-        return new_list        
+        return new_list 
+
+        
+
+
+        
