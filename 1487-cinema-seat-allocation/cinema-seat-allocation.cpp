9db274bcfaa57d95901dd629217a37608c719bf9
Goutham G <156352020+Goutham-Gourabathuni@users.noreply.github.com>
2026-08-19 16:33:38 +0530

    Time: 18 ms (81.93%) | Memory: 59 MB (90.20%) - LeetSync

---
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+        int left = 0b11110000, middle = 0b11000011, right = 0b00001111;
+        unordered_map<int, int> occupied;
+        for (auto& seat : reservedSeats) {
+            if (seat[1] >= 2 && seat[1] <= 9) {
+                occupied[seat[0]] |= (1 << (seat[1] - 2));
+            }
+        }
+        int ans = (n - occupied.size()) * 2;
+        for (auto& [row, bitmask] : occupied) {
+            if ((bitmask | left) == left || (bitmask | middle) == middle || (bitmask | right) == right) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
